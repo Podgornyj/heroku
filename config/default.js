62a284ea29d77df979d0f5c659b57db07e34c87c
Anton Podgornyj <podgornyjanton@gmail.com>
2017-06-05 04:54:45 +0300

    fix

--- config/default.js
+++ config/default.js
@@ -7,7 +7,7 @@ module.exports = {
   secret:   'mysecret',
   mongoose: {
       // uri:     'mongodb://localhost/app',
-       uri:     'mongodb://admin:admin@cluster0-shard-00-00-h3esm.mongodb.net:27017,cluster0-shard-00-01-h3esm.mongodb.net:27017,cluster0-shard-00-02-h3esm.mongodb.net:27017/testDataBase?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin',
+       uri:     'mongodb://Test:Test1@cluster0-shard-00-00-h3esm.mongodb.net:27017,cluster0-shard-00-01-h3esm.mongodb.net:27017,cluster0-shard-00-02-h3esm.mongodb.net:27017/testDataBase?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin',
     options: {
       server: {
         socketOptions: {
