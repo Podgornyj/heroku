3839e986aecf99f1e0ce375801c2466b745ad89d
Anton Podgornyj <podgornyjanton@gmail.com>
2017-06-07 00:03:31 +0300

    fix

--- index.js
+++ index.js
@@ -1,76 +1,40 @@
-if (process.env.TRACE) {
-  require('./libs/trace');
-}
+let passport = require('passport');
 
 const Koa = require('koa');
 const app = new Koa();
 
 const config = require('config');
-
-// keys for in-koa KeyGrip cookie signing (used in session, maybe other modules)
-app.keys = [config.secret];
+const mongoose = require('./libs/mongoose');
 
 const path = require('path');
 const fs = require('fs');
 
-const handlers = fs.readdirSync(path.join(__dirname, 'handlers')).sort();
+app.keys = [config.get('secret')];
 
-handlers.forEach(handler => require('./handlers/' + handler).init(app));
-
-// ---------------------------------------
+const handlers = fs.readdirSync(path.join(__dirname, 'middlewares')).sort();
+handlers.forEach(handler => require('./middlewares/' + handler).init(app));
 
 const Router = require('koa-router');
 
 const router = new Router();
 
-let clients = [];
-
-router.get('/subscribe', async (ctx, next) => {
-
-  ctx.set('Cache-Control', 'no-cache,must-revalidate');
-  const promise = new Promise((resolve, reject) => {
-    clients.push(resolve);
-
-    ctx.res.on('close', function() {
-      clients.splice(clients.indexOf(resolve), 1);
-      const error = new Error('Connection closed');
-      error.code = 'ECONNRESET';
-      reject(error);
-    });
-
-  });
-
-  let message;
-
-  try {
-    message = await promise;
-  } catch(err) {
-    if (err.code === 'ECONNRESET') return;
-    throw err;
-  }
-
-  // console.log('DONE', message);
-  ctx.body = message;
-
-});
-
-router.post('/publish', async (ctx, next) => {
-  const message = ctx.request.body.message;
-
-  if (!message) {
-    ctx.throw(400);
-  }
-
-  clients.forEach(function(resolve) {
-    resolve(String(message));
-  });
-
-  clients = [];
-
-  ctx.body = 'ok';
-
-});
+// router.get('/', require('./routes/frontpage').get);
+// router.get('/registration', require('./routes/registration').get);
+// router.post('/registration', require('./routes/registration').post);
+// router.post('/login', require('./routes/login').post);
+// router.post('/logout', require('./routes/logout').post);
+// router.get('/confirm/:id/:token', require('./routes/confirm').get);
+// router.get('/resendconfirm/:token', require('./routes/resendconfirm').get);
+//
+// router.get('/login/google', passport.authenticate('google', {
+// 	 display: 'popup',
+//      scope:   ['email']
+// }));
+// router.get('/oauth/google/collback', passport.authenticate('google',{
+// 	successRedirect : '/',
+//     failureRedirect : '/'
+// }));
 
 app.use(router.routes());
 
-app.listen(config.get('port'));
+app.listen(3000);
--- middlewares/08-multipartParser.js
+++ middlewares/08-multipartParser.js
@@ -5,29 +5,31 @@
 // can introduce config to ignore them here
 
 const busboy = require('co-busboy');
+const convert = require('koa-convert');
 
-exports.init = app => app.use(async (ctx, next) => {
+
+exports.init = app => app.use(convert(function* (next) {
   // the body isn't multipart, so busboy can't parse it
-  if (!ctx.request.is('multipart/*')) {
-    await next();
-    return;
+  if (!this.request.is('multipart/*')) {
+    return yield* next;
   }
 
-  const parser = busboy(ctx, {
+  const parser = busboy(this, {
     autoFields: true
   });
 
   let fileStream;
 
-  while (fileStream = await parser) {
+  while (fileStream = yield parser) {
+    // filesStream - stream with file
     // autoFields => part is a file
     // specific handlers know how to handle the file, not us
     // alt: can auto-save to disk
-    ctx.throw(400, "Files are not allowed here");
+    this.throw(400, "Files are not allowed here");
   }
 
   // copy normal fields from parser to ctx.request.body
-  const body = ctx.request.body;
+  const body = this.request.body;
 
   for (let [name, val, fieldnameTruncated, valTruncated] of parser.fields) {
     if (body[name]) { // same value already exists
@@ -40,5 +42,5 @@ exports.init = app => app.use(async (ctx, next) => {
     }
   }
 
-  await next();
-});
+  yield* next;
+}));
