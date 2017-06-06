6267a598c5c3a9aaeddc836434e953c667edc46f
Anton Podgornyj <podgornyjanton@gmail.com>
2017-06-07 00:26:36 +0300

    fix

--- routes/frontpage.js
+++ routes/frontpage.js
@@ -1,5 +1,5 @@
 exports.get = async function(ctx, next) {
-    ctx.body = '123';
+    tx.body = ctx.render('login');
     // if (ctx.isAuthenticated()) {
   // 	if(ctx.req.user.emailConfirm){
   //   ctx.body = ctx.render('welcome');
